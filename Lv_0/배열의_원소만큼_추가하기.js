c0ea82a6a233ce8ef6c044a53f1787129c13410e
hwuiinn <gnldls8406@gmail.com>
2024-07-16 23:18:21 +0900

    Lv0 : my string

---
+++ Lv_0/배열의_원소만큼_추가하기.js
@@ -0,0 +1,32 @@
+// 아무 원소도 들어있지 않은 빈 배열 X가 있습니다.
+// 양의 정수 배열 arr가 매개변수로 주어질 때,
+// arr의 앞에서부터 차례대로 원소를 보면서 원소가 a라면 X의 맨 뒤에 a를 a번 추가하는 일을 반복한 뒤의 배열 X를 return 하는 solution 함수를 작성해 주세요.
+
+// 1.
+
+// function solution(arr) {
+//   const resultArr = [];
+
+//   // 각 arr 배열의 요소에 대해 반복합니다.
+//   for (let i = 0; i < arr.length; i++) {
+//     // arr[i] 값만큼 반복하여 resultArr에 추가합니다.
+//     for (let j = 0; j < arr[i]; j++) {
+//       console.log("arr[i] : ", arr[i]);
+//       resultArr.push(arr[i]);
+//     }
+//   }
+
+//   console.log(resultArr);
+
+//   // 최종적으로 채워진 배열 resultArr을 반환합니다.
+//   return resultArr;
+// }
+
+function solution(arr) {
+  console.log("--------------", new Array(10).fill(1));
+  const fillArr = arr.map((value) => new Array(value).fill(value));
+
+  return [].concat(...fillArr);
+}
+
+solution([5, 1, 4]); // 	[5, 5, 5, 5, 5, 1, 4, 4, 4, 4]
