6becb859a595a851b8654c90acf2cb2162016a5f
hwuiinn <gnldls8406@gmail.com>
2024-08-07 22:44:11 +0900

    Lv0 : 배열 만들기 1

---
+++ Lv_0/배열만들기1.js
@@ -0,0 +1,15 @@
+// 정수 n과 k가 주어졌을 때, 1 이상 n이하의 정수 중에서 k의 배수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+
+function solution(n, k) {
+  const quotient = n / k;
+
+  const arr = [];
+
+  for (let i = 1; i <= quotient; i++) {
+    arr.push(i * k);
+  }
+
+  return arr;
+}
+
+solution(15, 5); // [5, 10, 15]
