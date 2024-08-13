b116e44072114a2de9f625ebc2a8b0ecfd895cfa
hwuiinn <gnldls8406@gmail.com>
2024-08-13 23:52:35 +0900

    Lv0 : 두 수의 연산값 비교

---
+++ Lv_0/두_수의_연산값_비교하기.js
@@ -0,0 +1,24 @@
+// 연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+// 12 ⊕ 3 = 123
+// 3 ⊕ 12 = 312
+// 양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
+
+// 단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
+
+function solution(a, b) {
+  const addNum = Number(a.toString() + b.toString());
+
+  const multiplyArr = [a, b];
+  const multiplyNum = 2 * multiplyArr[0] * multiplyArr[1];
+
+  if (addNum > multiplyNum) {
+    return addNum;
+  } else if (addNum < multiplyNum) {
+    return multiplyNum;
+  } else if (addNum === multiplyNum) {
+    return addNum + multiplyNum;
+  }
+}
+
+solution(2, 91); // 364
+// solution(91, 2); // 912
