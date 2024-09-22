9e0d0436fa0de393a4d79edbd0604a146d46eddf
hwuiinn <gnldls8406@gmail.com>
2024-09-22 23:11:03 +0900

    Lv0: 덧셈식 출력하기

---
+++ Lv_0/덧셈식_출력하기.js
@@ -0,0 +1,16 @@
+// 두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = line.split(" ");
+}).on("close", function () {
+  let sum = Number(input[0]) + Number(input[1]);
+  console.log(`${input[0]} + ${input[1]} = ${sum}`);
+});
