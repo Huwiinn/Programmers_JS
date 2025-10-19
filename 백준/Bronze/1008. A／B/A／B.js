e3f9de4977b90070eafe1ad45fff395c81d469c1
박휘인 <82766211+Huwiinn@users.noreply.github.com>
2025-10-19 15:56:30 +0900

    [Bronze V] Title: A/B, Time: 92 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString()
+  .trim()
+  .split(" ")
+  .map((num) => Number(num));
+
+const A = input[0];
+const B = input[1];
+
+console.log(A / B);
