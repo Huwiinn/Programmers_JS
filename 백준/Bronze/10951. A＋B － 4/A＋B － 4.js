9d112747efd7677dbbd0c7da849d913ab765425b
박휘인 <82766211+Huwiinn@users.noreply.github.com>
2025-10-19 16:41:55 +0900

    [Bronze V] Title: A+B - 4, Time: 100 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim()
+  .split("\n")
+  .map((item) => item);
+
+for (let i = 0; i < input.length; i++) {
+  const [A, B] = input[i].split(" ").map(Number);
+  console.log(A + B);
+}
